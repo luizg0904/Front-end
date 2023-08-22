2a4b9f50e5208b66a54ae6a3af1392ebf15fd85c
Luiz Guilherme <123389256+luizg0904@users.noreply.github.com>
2023-08-22 10:52:43 -0300

    Testes

---
+++ TesteWhile/TesteWhile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i=0;
+    while(i<10)
+    {
+        printf("Hello world!\n");
+        i++;
+    }
+    printf("\n\nSAI DO FOR\n\n");
+    return 0;
+}
