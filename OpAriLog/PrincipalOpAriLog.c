41a1c2eb49444359a1f69430534cc769a4c68943
Luiz Guilherme <123389256+luizg0904@users.noreply.github.com>
2023-08-29 10:50:54 -0300

    Testes

---
+++ OpAriLog/PrincipalOpAriLog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c;
+{
+    a = 3;
+    a +=5;
+    a = a + 5;
+    printf("a=%d\n",a);
+}
+return 0;
+
+}
---
+++ ex04/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,y;
+    x= (5>3);
+    y = (3>5);
+
+    if(x && y) printf("Verdade/n");
+    else printf("Falsidade/n");
+
+    return 0;
+}
